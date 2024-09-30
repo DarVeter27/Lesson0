388660360c7f9214c268eb75d7a5cf5a1af2f39b
DarVeter27 <alexandrgrei27@gmail.com>
2024-09-30 12:49:43 +0700

    Add files via upload

---
+++ Lesson01.py
@@ -0,0 +1,23 @@
+print(5 // 5) # // - целочисленное деление, / - деление
+print(5 % 5) # % - поиск остатока от деления
+print(5 ** 2) # ** - возведение в степень
+print (type(5))
+print(2.0) # число с плавающей запятой, дробное
+print(2.0 + 4.6)
+print(2.0 * 5)
+print (type(2.0))
+print('Hello "World!"') # str (string) - строка, текст
+print("Hello 'World!'")
+print (type('Hello World!'))
+print("Hello," + "World!") # concatenete - сложение строк текста
+print("Hello, " + "World!")
+print(True, False) # bool (boolean) - логический тип данных
+print (type(True), type(False))
+print(5 > 10, 5 < 10, 5 <= 5, 5 >= 5, 5 == 5, 5 != 5)
+print(1, 2, 3, 4, 5, 'Hello "World!"', True)
+print(5 != 5 and 5 < 10) # and - строгий оператор, оба выражения должны быть True, False
+print(5 == 5 and 5 < 10)
+print(5 == 5 or 5 < 10) # or - не строгий оператор, одно из выражений должно быть True, False
+print(5 != 5 or 5 < 10)
+print('5')
+print(type(int('5'))) # принудительнй перевод в различные типы данных
