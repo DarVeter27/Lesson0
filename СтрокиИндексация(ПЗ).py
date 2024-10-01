8c041ad220c63e7aadbcecc0ab6d587d1fe6739c
DarVeter27 <alexandrgrei27@gmail.com>
2024-10-01 13:34:45 +0700

    Add files via upload

---
+++ СтрокиИндексация(ПЗ).py
@@ -0,0 +1,11 @@
+name = ('Alexandr')
+print('Hello, ' + name)
+print('Hello, ''Alexandr')
+print(('Hello, ''Alexandr; ') * 5)
+print(name[0]) # вывод символа строки с индексом 0
+print(name[-1])
+print(name[0:4]) # вывод символов строки в диапазоне индексов от 0 до 4 (не включая 4)
+print(name[0:4:2]) # вывод символов строки в диапазоне индексов от 0 до 4 с шагом в 2
+print(name[:4])
+print(name[3:])
+print(name[::-1])
