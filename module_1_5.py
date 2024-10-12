053e8176dfbfa76ee102a033768649d471a54356
DarVeter27 <alexandrgrei27@gmail.com>
2024-10-12 20:47:07 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2.5, 3, "Alexandr", True, False
+print(immutable_var)
+# immutable_var [3] = 300 - кортеж не поддерживает функцию изменения значения элементов находящихся внутри него
+
+mutable_list = [1, 2.5, 3, "Alexandr", True]
+print(mutable_list)
+mutable_list [4] = 600
+print(mutable_list)
